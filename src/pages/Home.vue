6f18e34df50194dd24286b79b8cf45c3827e3828
MelodyCcc <10981481+melodyccc@user.noreply.gitee.com>
2022-05-19 22:16:09 +0800

    完成了登录功能

---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <el-button type="danger" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        logout(){
+            window.sessionStorage.clear();
+            this.$router.push('/login')
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
